25508ed984d9f17505c50cc4afc12b0c03165185
jongchun <jrah@my.bcit.ca>
2017-02-14 14:31:29 -0800

    Add files via upload

---
+++ UsersAndRolesDemo/UsersAndRolesDemo/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Routing;
+using UsersAndRolesDemo;
+
+namespace UsersAndRolesDemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AntiForgeryConfig.SuppressIdentityHeuristicChecks = true;
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        void Application_PostAuthenticateRequest() {
+            if (User.Identity.IsAuthenticated) {
+                var name = User.Identity.Name; // Get current user name.
+ 
+                MyDbEntities context = new MyDbEntities();
+                var user  = context.AspNetUsers.Where(u=>u.UserName == name).FirstOrDefault();
+                IQueryable<string> roleQuery = from  u in context.AspNetUsers 
+                            from  r in u.AspNetRoles 
+                            where u.UserName == Context.User.Identity.Name
+                            select r.Name;
+                string[] roles = roleQuery.ToArray();
+
+                HttpContext.Current.User = Thread.CurrentPrincipal =
+                                           new GenericPrincipal(User.Identity, roles);
+            }
+        }
+        protected void Application_BeginRequest()
+        {
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+            Response.Cache.SetNoStore();
+        }
+
+    }
+}
