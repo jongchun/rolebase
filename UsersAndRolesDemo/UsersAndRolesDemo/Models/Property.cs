ace7ba75d6906015c62b6d37973e5d93e1d0e70a
Carlos <carlokoz@gmail.com>
2017-02-21 14:10:38 -0800

    creation of Property model

---
+++ UsersAndRolesDemo/UsersAndRolesDemo/Models/Property.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UsersAndRolesDemo.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string summary { get; set; }
+        public string propertyType { get; set; }
+        public Nullable<int> numBedrooms { get; set; }
+        public Nullable<int> numWashrooms { get; set; }
+        public Nullable<int> kitchen { get; set; }
+        public Nullable<decimal> baseRate { get; set; }
+        public string address { get; set; }
+        public string builtYear { get; set; }
+        public string smokingAllowed { get; set; }
+        public string maxNumberGuests { get; set; }
+        public Nullable<System.DateTime> availableDates { get; set; }
+        public string dimensions { get; set; }
+    }
+}
