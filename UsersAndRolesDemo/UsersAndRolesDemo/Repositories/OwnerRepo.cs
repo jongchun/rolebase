0fbf66986be560552df9dd8ce9a525190197f43c
JC Rah <jrah@my.bcit.ca>
2017-02-28 11:44:00 -0800

    fixed AccountRepo

--- UsersAndRolesDemo/UsersAndRolesDemo/Repositories/OwnerRepo.cs
+++ UsersAndRolesDemo/UsersAndRolesDemo/Repositories/OwnerRepo.cs
@@ -13,51 +13,6 @@ namespace UsersAndRolesDemo.Repositories
     {
         private MyDbEntities db = new MyDbEntities();
 
-        public Boolean UpdateOwner(OwnerProfileVM model)
-        {
-            AspNetUser user = db.AspNetUsers
-                        .Where(a => a.Id == model.Id).FirstOrDefault();
-
-            var userStore = new UserStore<IdentityUser>();
-            UserManager<IdentityUser> manager = new UserManager<IdentityUser>(userStore);
-
-            IdentityResult result = null;
-            if (model.CurrentPassword != null && model.Password != null && model.ConfirmPassword != null)
-            {
-                result = manager.ChangePassword(model.Id, model.CurrentPassword, model.Password);
-            }
-            if (result == null || result.Succeeded)
-            {
-                if (model.ProfilePicture != null)
-                {
-                    user.profilePicture = model.ProfilePicture;
-                }
-                user.UserName = model.UserName;
-                user.firstName = model.FirstName;
-                user.lastName = model.LastName;
-                user.Email = model.Email;
-                user.PhoneNumber = model.PhoneNumber;
-                user.cellPhone = model.CellPhone;
-                user.address = model.Address;
-                user.city = model.City;
-                user.region = model.Region;
-                user.postalCode = model.PostalCode;
-                user.directDepositRouting = model.DirectDepositRouting;
-                user.directDepositBank = model.DirectDepositBank;
-                user.directDepositAccount = model.DirectDepositAccount;
-
-                db.Entry(user).State = EntityState.Modified;
-                try
-                {
-                    db.SaveChanges();
-                }
-                catch (Exception)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
     }
 }
     
