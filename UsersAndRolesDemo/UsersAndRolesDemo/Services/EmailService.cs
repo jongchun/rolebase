18bec07b198f16e3100420a1c639f1b3f676ad5f
JC Rah <rahjc.ca@gmail.com>
2017-02-28 23:26:41 -0800

    sendgrid not working yet

--- UsersAndRolesDemo/UsersAndRolesDemo/Services/EmailService.cs
+++ UsersAndRolesDemo/UsersAndRolesDemo/Services/EmailService.cs
@@ -11,11 +11,10 @@ namespace UsersAndRolesDemo.Services
 {
     public class EmailService
     {
-        private string apiKey = "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";
         private string adminEmail = "seatoskycabins@gmail.com";
         public void SendEmail(string email, string subject, string content)
         {
-            //var apiKey = "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";
+            var apiKey = "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";
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
@@ -30,6 +29,7 @@ namespace UsersAndRolesDemo.Services
 
         public void ContactUS(Contact contact)
         {
+            var apiKey = "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";
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
@@ -38,7 +38,7 @@ namespace UsersAndRolesDemo.Services
                 PlainTextContent = contact.Message,
                 HtmlContent = contact.Message
             };
-            msg.AddTo(new EmailAddress(adminEmail, "To Admin"));
+            msg.AddTo(new EmailAddress("seatoskycabins@gmail.com", "To Admin"));
             client.SendEmailAsync(msg);
         }
     }
