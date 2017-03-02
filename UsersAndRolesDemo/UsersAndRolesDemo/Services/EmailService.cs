c99337c18269468d1cddbc447c59e18f9d4254cf
jongchun <jrah@my.bcit.ca>
2017-03-02 09:13:27 -0800

    Update EmailService.cs
    removed api key

--- UsersAndRolesDemo/UsersAndRolesDemo/Services/EmailService.cs
+++ UsersAndRolesDemo/UsersAndRolesDemo/Services/EmailService.cs
@@ -14,7 +14,7 @@ namespace UsersAndRolesDemo.Services
         private string adminEmail = "seatoskycabins@gmail.com";
         public void SendEmail(string email, string subject, string content)
         {
-            var apiKey = "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";
+            var apiKey = "";
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
@@ -29,7 +29,7 @@ namespace UsersAndRolesDemo.Services
 
         public void ContactUS(Contact contact)
         {
-            var apiKey = "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";
+            var apiKey = "";
             var client = new SendGridClient(apiKey);
             var msg = new SendGridMessage()
             {
