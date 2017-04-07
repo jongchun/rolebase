ce1e200c1326c66a289c8b0d136ad8711d7ab979
Carlos Rado <carlokoz@gmail.com>
2017-04-07 11:13:08 -0700

    Creation of custom IPN class for reservation payments

---
+++ UsersAndRolesDemo/UsersAndRolesDemo/Models/IPN.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UsersAndRolesDemo.Models
+{
+    public class IPN
+    {
+        [Key]
+        [Display(Name = "Transaction ID")]
+        public string transactionID { get; set; }
+        [Display(Name = "Transaction Time (UTC)")]
+        public DateTime txTime { get; set; }
+        [Display(Name = "Session ID")]
+        public string custom { get; set; }
+        [Display(Name = "First Name")]
+        public string fname { get; set; }
+        [Display(Name = "Last Name")]
+        public string lname { get; set; }
+        [Display(Name = "Buyer Email")]
+        public string buyerEmail { get; set; }
+        [Display(Name = "Total Reservations")]
+        public string quantity { get; set; }
+        [Display(Name = "Transaction Amount")]
+        [DataType(DataType.Currency)]
+        public decimal amount { get; set; }
+        [Display(Name = "Payment Status")]
+        public string paymentStatus { get; set; }
+
+    }
+}
