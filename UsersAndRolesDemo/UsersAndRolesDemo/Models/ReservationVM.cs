1eea16859ed6d175876d51faab6ce6e8749645d0
Carlos Rado <carlokoz@gmail.com>
2017-04-12 15:46:06 -0700

    created the Reservation view model for STSC's rental form.  This will also reflect changes made to the sites ERD structure that has been adjusted for PayPal implimentation.

---
+++ UsersAndRolesDemo/UsersAndRolesDemo/Models/ReservationVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UsersAndRolesDemo.Models
+{
+    public class ReservationVM
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string RenterFirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string RenterLastName { get; set; }
+
+        [Required]
+        [Display(Name = "Reservation Start")]
+        public string StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "Reservation End")]
+        public int EndDate { get; set; }
+
+        //[Required]
+        //[Display(Name = "Base Rate Per Day")]
+        //public decimal BaseRate { get; set; }
+    }
+}
