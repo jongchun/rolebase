31229f315fa140340d3cdd16c8626fd9cc31d7bd
jongchun <jrah@my.bcit.ca>
2017-04-18 13:06:02 -0700

    updated

--- UsersAndRolesDemo/UsersAndRolesDemo/Global.asax.cs
+++ UsersAndRolesDemo/UsersAndRolesDemo/Global.asax.cs
@@ -15,7 +15,7 @@ namespace UsersAndRolesDemo
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig2.Register);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings
             .ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
 
