e66181acfaef39e8dcfe949832c61ede2effae45
JC Rah <rahjc.ca@gmail.com>
2017-04-22 14:45:43 -0700

    updated app.js and info.cshtml

--- UsersAndRolesDemo/UsersAndRolesDemo/Scripts/app.js
+++ UsersAndRolesDemo/UsersAndRolesDemo/Scripts/app.js
@@ -1,6 +1,6 @@
 ﻿/// <reference path="jquery-3.1.1.js" />
-//var serviceUrl = 'http://localhost:58046/api/Properties';
-var serviceUrl = './api/Properties'
+var serviceUrl = 'http://www.jcssd.me/seatoskycabins/api/Properties';
+// var serviceUrl = './api/Properties'
 
 function sendRequest() {
     //   jQuery.support.cors = true;
@@ -21,24 +21,25 @@ function sendRequest() {
 function callback(val) {
     $("#value1").replaceWith("<dl  id='properties' />");
     //var str =  "propertyTitle: " + val.title  + "propertySummary: " + val.summary + " Nummer Bedrooms: " + val.numBedrooms + " Nummer Washrooms: " + val.numWashrooms + " Kitchen: " + val.kitchen + val.smokingAllowed + " max Number Guests" + val.maxNumberGuests + " Dimensions:" + val.dimensions;
-    var str = "propertyTitle: " + val.title
-    var strSummary = "propertySummary: " + val.summary
-    var strBedroom = " Nummer Bedrooms: " + val.numBedrooms
-    var strWashroom = val.numBedrooms + " Nummer Washrooms: " + val.numWashrooms
-    var strKitchen = " Kitchen: " + val.kitchen
+    var str = val.title
+    var strSummary = val.summary
+    var strBedroom = "Nummer Bedrooms: " + val.numBedrooms
+    var strWashroom = "Nummer Washrooms: " + val.numWashrooms
+    var strKitchen = "Kitchen: " + val.kitchen
     var strSmoking = "smokingAllowed" + val.smokingAllowed
     var strMaxGuests = "max Number Guests" + val.maxNumberGuests
-    var strDimensions = " Dimensions:" + val.dimensions
-    $('<div>').appendTo($('#properties'));
-    $('<li/>', { text: str }).appendTo($('#properties'));
-    $('<li/>', { text: strSummary }).appendTo($('#properties'));
-    $('<li/>', { text: strBedroom }).appendTo($('#properties'));
-    $('<li/>', { text: strWashroom }).appendTo($('#properties'));
-    $('<li/>', { text: strKitchen }).appendTo($('#properties'));
-    $('<li/>', { text: strSmoking }).appendTo($('#properties'));
-    $('<li/>', { text: strMaxGuests }).appendTo($('#properties'));
-    $('<li/>', { text: strDimensions }).appendTo($('#properties'));
-    $('<li/>').text("<a href='/Home/Details/" + data.Id + "'" + data.title + "</a>");
+    var strDimensions = "Dimensions:" + val.dimensions
+
+    $('<dt/>', { text: str }).appendTo($('#properties'));
     $('<br/>').appendTo($('#properties'));
-    $('<div/>').appendTo($('#properties'));
+    $('<dd/>', { text: strSummary }).appendTo($('#properties'));
+    $('<br/>').appendTo($('#properties'));
+    $('<dd/>', { text: strBedroom }).appendTo($('#properties'));
+    $('<dd/>', { text: strWashroom }).appendTo($('#properties'));
+    $('<dd/>', { text: strKitchen }).appendTo($('#properties'));
+    $('<dd/>', { text: strSmoking }).appendTo($('#properties'));
+    $('<dd/>', { text: strMaxGuests }).appendTo($('#properties'));
+    $('<dd/>', { text: strDimensions }).appendTo($('#properties'));
+    //$('<li/>').text("<a href='/Home/Details/" + data.Id + "'" + data.title + "</a>");
+    $('<br/>').appendTo($('#properties'));
 }
